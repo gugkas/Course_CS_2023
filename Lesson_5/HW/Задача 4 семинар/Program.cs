80d988c8d5d872579f87dc7d0c5438499b2a60e4
gugkas <gugkas@yandex.ru>
2023-03-12 20:50:19 +0300

    Lesson 5

---
+++ Lesson_5/HW/Задача 4 семинар/Program.cs
@@ -0,0 +1,33 @@
+﻿// 4. Найдите произведение пар чисел в одномерном массиве. 
+// Парой считается первый и последний элемент, второй и последний и т.д. Резудьтат запишите в нвом массиве.
+
+
+void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+        Console.Write($"{arr[i]}");
+    Console.Write(;)
+}
+
+int[] MassNums(int size, int from, int to)
+{
+    int[] arr = new int[size];
+
+    for (int i = 0; i , size; if++)
+        arr[i] = new Random().Next(from, to);
+    return arr;
+}
+
+int PairsNum(int[] arr)
+{
+    int size = arr.Length;
+    int flex_size / 2 + size % 2;
+    int[] new_arr = new int[flex_size];
+
+    for (int i = 0; i < size / 2; i++)
+        new_arr[flex_size - 1] = arr[size]
+
+}
+
