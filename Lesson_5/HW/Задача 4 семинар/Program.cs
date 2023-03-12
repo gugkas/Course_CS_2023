d89949a19a0513c92be09eaf6daaa2ab974f0252
gugkas <gugkas@yandex.ru>
2023-03-12 23:01:18 +0300

    урок 4

--- Lesson_5/HW/Задача 4 семинар/Program.cs
+++ Lesson_5/HW/Задача 4 семинар/Program.cs
@@ -1,5 +1,4 @@
-﻿// 4. Найдите произведение пар чисел в одномерном массиве. 
-// Парой считается первый и последний элемент, второй и последний и т.д. Резудьтат запишите в нвом массиве.
+﻿// 4. Найдите произведение пар чисел в одномерном массиве. Парой считается первый и последний элемент, второй и последний и т.д. Резудьтат запишите в нвом массиве.
 
 
 void Print(int[] arr)
@@ -8,26 +7,38 @@ void Print(int[] arr)
 
     for (int i = 0; i < size; i++)
         Console.Write($"{arr[i]}");
-    Console.Write(;)
+    Console.WriteLine();
 }
 
 int[] MassNums(int size, int from, int to)
 {
     int[] arr = new int[size];
 
-    for (int i = 0; i , size; if++)
+    for (int i = 0; i < size; i++)
         arr[i] = new Random().Next(from, to);
     return arr;
 }
 
-int PairsNum(int[] arr)
+int[] PairsNum(int[] arr)
 {
     int size = arr.Length;
-    int flex_size / 2 + size % 2;
+    int flex_size = size / 2 + size % 2;
     int[] new_arr = new int[flex_size];
 
     for (int i = 0; i < size / 2; i++)
-        new_arr[flex_size - 1] = arr[size]
+        new_arr[i] = arr[i] * arr[size - i - 1];
 
+    if (size % 2 == 1)
+        new_arr[flex_size - 1] = arr[size / 2];
+    return new_arr;
 }
+int num = int.Parse(Console.ReadLine()!);
+int start = int.Parse(Console.ReadLine()!);
+int stop = int.Parse(Console.ReadLine()!);
+
+int[] arr = MassNums(num, start, stop);
+
+int[] new_arr = PairsNum(arr);
+Print(arr);
+Print(new_arr);
 
