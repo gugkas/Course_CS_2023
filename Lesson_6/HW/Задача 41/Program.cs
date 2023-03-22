9892ad897c51d1611da6e94c8fa64c85530244fa
gugkas <gugkas@yandex.ru>
2023-03-22 19:03:39 +0300

    Lesson_7

--- Lesson_6/HW/Задача 41/Program.cs
+++ Lesson_6/HW/Задача 41/Program.cs
@@ -1,6 +1,6 @@
 ﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
 
-Console.Write("Введите элементы через пробел: ");
+Console.Write("Введите элементы через пробел:");
 int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 int count = 0;
 
