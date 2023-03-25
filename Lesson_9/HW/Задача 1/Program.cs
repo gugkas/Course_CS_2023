153be2591183a2141b39849ace45813e8790df1f
gugkas <gugkas@yandex.ru>
2023-03-25 14:15:29 +0300

    Задача 1

---
+++ Lesson_9/HW/Задача 1/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задайте значения M и N. Напишите программу, которая выведет все чётные
+//натуральные числа в промежутке от M до N с помощью рекурсии.
+
+
+
+Console.Write("Введите значение M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+void ValuesSet(int m, int n)
+{
+if (m > n)
+return;
+if (m % 2 == 0)
+{
+Console.Write($"{m}, ");
+}
+ValuesSet(m+1,n);
+
+}
+ValuesSet(m,n);
---
+++ Lesson_9/HW/Задача 2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму
+//натуральных элементов в промежутке от M до N с помощью рекурсии.
+
+Console.WriteLine("Введите  число M:");
+int numberM = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите  число N:");
+int numberN = int.Parse(Console.ReadLine());
+
+void NumberSum(int numberM, int numberN, int sum)
+{
+    if (numberM > numberN)
+    {
+        Console.WriteLine($"Сумма  элементов в промежутке от M до N: {sum}");
+        return;
+    }
+    sum = sum + (numberM++);
+    NumberSum(numberM, numberN, sum);
+}
+
+NumberSum(numberM, numberN, 0);
